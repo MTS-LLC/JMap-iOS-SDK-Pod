ffe39cdf6cb36f846fcc3e4fc346b27b7a20942e
Matt Willis <mewillis21@gmail.com>
2024-08-05 07:24:11 -0400

    podfile: updated podfile to point to JMapiOSSDK.podspec

--- Example/Pods/Target Support Files/JMapiOSSDK4.0/JMapiOSSDK4.0-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_JMapiOSSDK4_0 : NSObject
-@end
-@implementation PodsDummy_JMapiOSSDK4_0
-@end
--- Example/Pods/Target Support Files/JMapiOSSDK4.0/JMapiOSSDK4.0-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double JMapiOSSDK4_0VersionNumber;
-FOUNDATION_EXPORT const unsigned char JMapiOSSDK4_0VersionString[];
-
